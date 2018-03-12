dc0bdaa38b89fe3fad0625bf96413f63b1774bb8
橋爪友莉子 <yuyu1.run@icloud.com>
2018-03-12 11:48:02 +0900

    ホーム画面の変更

--- manuscript_service/config/routes.rb
+++ manuscript_service/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :manuscripts
+  root to: 'manuscripts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'view/show_result'
 end
